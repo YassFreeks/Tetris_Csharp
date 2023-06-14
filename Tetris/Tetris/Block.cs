054e821ed80acb84bdaafc0d67c236e291b83508
YassFreeks <y.moutaouakil@hotmail.com>
2023-06-14 20:22:34 +0200

    Loading ...

--- Tetris/Tetris/Block.cs
+++ Tetris/Tetris/Block.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Tetris
 {
-    internal class Block
+    public abstract class Block // La classe abstraite "Block" définit trois membres abstraits : "Tiles", "StartOffset" et "Id". Ces membres doivent être implémentés dans les classes dérivées de "Block" pour définir les spécificités de chaque type de bloc. "Tiles" est un tableau bidimensionnel de positions représentant les tuiles du bloc dans différentes rotations. "StartOffset" est la position de départ du bloc. "Id" est un identifiant unique pour le bloc.
     {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        // La classe "Block" contient deux champs privés : "rotationState" de type entier et "offset" de type "Position". "rotationState" représente l'état de rotation actuel du bloc. "offset" représente le décalage du bloc par rapport à sa position de référence.
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions() // La méthode publique "TilePositions()" retourne un énumérable d'objets "Position". Cette méthode itère sur les positions des tuiles du bloc correspondant à l'état de rotation actuel (défini par "rotationState"). Pour chaque position "p" dans "Tiles[rotationState]", la méthode crée une nouvelle instance de la classe "Position" en ajoutant les valeurs de "p.Row" et "p.Column" à "offset.Row" et "offset.Column" respectivement, et la retourne à l'aide de l'instruction "yield return".
+
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        // Les méthodes "RotateCW()" et "RotateCCW()" permettent de faire pivoter le bloc dans le sens des aiguilles d'une montre (CW) et dans le sens inverse des aiguilles d'une montre (CCW) respectivement. Dans "RotateCW()", "rotationState" est incrémenté de 1, et si la nouvelle valeur dépasse la longueur du tableau "Tiles", elle est ramenée à 0 en utilisant l'opération de modulo (%).
+        // Dans "RotateCCW()", "rotationState" est décrémenté de 1, et si la valeur actuelle est 0, elle est définie sur la longueur du tableau "Tiles" moins 1 pour assurer une rotation correcte dans le sens inverse.
+
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        // La méthode "Move()" permet de déplacer le bloc en ajustant les valeurs de "offset.Row" et "offset.Column" en fonction des nombres de lignes (rows) et de colonnes (columns) spécifiés.
+        // La méthode "Reset()" réinitialise l'état du bloc en rétablissant "rotationState" à 0 et en remettant "offset.Row" et "offset.Column" à leurs valeurs initiales définies par "StartOffset.Row" et "StartOffset.Column".
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
     }
 }
+
+
+
+// En résumé, ce code permet de créer une structure de base pour les blocs du jeu Tetris, en définissant des fonctionnalités communes à tous les types de blocs. Les classes dérivées pour chaque type de bloc peuvent implémenter les membres abstraits spécifiques à leur propre géométrie et configuration. Cela facilite l'extensibilité et la gestion des blocs dans le jeu Tetris, en séparant la logique générale des blocs de leurs caractéristiques spécifiques.
---
+++ Tetris/Tetris/IBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+
+namespace Tetris
+{
+    public class IBlock : Block // Le code est contenu dans le namespace "Tetris". La classe "IBlock" hérite de la classe abstraite "Block" et en hérite toutes les fonctionnalités et membres.
+    {
+        private readonly Position[][] tiles = new Position[][] //La classe "IBlock" définit un tableau bidimensionnel "tiles" de positions représentant les tuiles du bloc "I-block" dans différentes rotations. Chaque élément du tableau correspond à une rotation spécifique du bloc, et chaque rotation est représentée par un tableau de positions. Chaque position est créée à l'aide de l'initialiseur d'objets avec des coordonnées x et y.
+        {
+            new Position[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new Position[] { new(0,2), new(1,2), new(2,2), new(3,2) },
+            new Position[] { new(2,0), new(2,1), new(2,2), new(2,3) },
+            new Position[] { new(0,1), new(1,1), new(2,1), new(3,1) }
+        };
+
+        // La propriété "Id" est redéfinie en tant que propriété en lecture seule dans la classe "IBlock" et retourne la valeur 1 pour identifier spécifiquement le bloc "I-block". La méthode "StartOffset" est redéfinie en tant que méthode protégée dans la classe "IBlock" et retourne une instance de la classe "Position" avec les coordonnées(-1, 3). Cela définit la position de départ du bloc "I-block". La méthode "Tiles" est redéfinie en tant que méthode protégée dans la classe "IBlock" et retourne le tableau "tiles" défini précédemment, représentant les différentes rotations du bloc "I-block".
+
+        public override int Id => 1;
+        protected override Position StartOffset => new Position(-1, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
+
+// En résumé, ce code définit la classe "IBlock" qui représente le type spécifique de bloc "I-block" dans le jeu Tetris. Il utilise l'héritage de la classe abstraite "Block" pour obtenir les fonctionnalités de base du bloc et redéfinit les membres abstraits pour spécifier les propriétés et les configurations spécifiques du bloc "I-block". Les rotations possibles et les positions des tuiles du bloc sont définies dans le tableau "tiles".
